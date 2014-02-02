1dcac248832b84eae263d02a435b41cfc811dcd4
Alex Stockwell <astockwell@gmail.com>
2014-02-02 10:45:12 -0700

    working prev/next buttons

--- config.rb
+++ config.rb
@@ -85,32 +85,31 @@ helpers do
         return current_resource.path.gsub(/#{current_iteration}/) { |match| current_iteration - 1 }
       end
     end
-    return "false"
+
+    return false
   end
 
   def next_link
-    num_of_iterations = Dir.glob(File.join('source', 'v*.html.erb')).size
+    num_of_iterations = Dir.glob(File.join('source', '*v*.html.erb')).size
 
     if current_resource.path.include? "index.html"
       if num_of_iterations > 0
         return "v2.html"
       else
-        return "false"
+        return false
       end
     end
 
-    if num_of_iterations == 1
-      if current_resource.path.include? "v2"
-        return "false"
-      end
-    elsif num_of_iterations >= 2
+    if num_of_iterations > 0
       current_iteration = Integer(current_resource.path.gsub(/(.*?v|\.html)/) { |match| "" })
-      if num_of_iterations < current_iteration - 1
+
+      if current_iteration < num_of_iterations
         return current_resource.path.gsub(/#{current_iteration}/) { |match| current_iteration + 1 }
       else
-        return "false"
+        return false
       end
     end
-    return "false"
+
+    return false
   end
 end
